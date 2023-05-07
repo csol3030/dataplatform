493c9da6c3214f17160e1f701746491da3c20159
Abhilash Pimpalkar <abhilash.p@anblicks.com>
2023-05-07 17:15:15 +0530

    dp-orchestrate - OCR Cloud(Azure) and Open Source Airflow Dag integration

--- dags/af_ocr_process.py
+++ dags/af_ocr_process.py
@@ -1,4 +1,4 @@
-import os, fnmatch, time, uuid
+import os, fnmatch, time, uuid, re
 import json, asyncio
 from datetime import datetime
 
@@ -14,13 +14,14 @@ from airflow.models.param import Param
 from airflow.utils.edgemodifier import Label
 from airflow.operators.python import PythonOperator
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 
 from azure.storage.blob import BlobServiceClient, BlobClient
 from azure.identity import AzureCliCredential
 from azure.keyvault.secrets import SecretClient
 
 import sys
-sys.path.append('/usr/local/airflow/include/scripts')
+sys.path.append("/usr/local/airflow/include/scripts")
 
 import ocr_utils
 
@@ -57,36 +58,78 @@ def cleanup_xcom(session=None, **context):
 
 def get_kv_secret(secret_name):
     fetched_secret = kv_client.get_secret(secret_name)
-    # print(fetched_secret.value)
     return fetched_secret.value
 
 adls_details = json.loads(get_kv_secret(KEYVAULT_ADLS_BLOB_SECRET))
 blob_service_client = BlobServiceClient.from_connection_string(
     conn_str=adls_details["connection_string"]
 )
-container_name = 'cont-datalink-dp-shared' 
-folder_path = 'OCR_DATA/'
 
-def download_blob_to_file(blob_service_client: BlobServiceClient, container_name): 
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob="OCR_DATA/AetnaMA_Smith_Jane_12.17.1950_OMWb.pdf") 
-    download_stream = blob_client.download_blob() 
-    bytes_data=download_stream.readall() 
+def download_blob_to_file(
+    blob_service_client: BlobServiceClient, container_name, blob_name
+):
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name, blob=blob_name
+    )
+    download_stream = blob_client.download_blob()
+    bytes_data = download_stream.readall()
     return bytes_data
 
-def get_ocr_details():
 
-    print("============inside get_ocr_details================")
+def get_ocr_details(**context):
+    print("============inside get_ocr_details================", context["params"])
     # formUrl = r"C:\Anblicks\Projects\Datalink\Utilities\org_file\AetnaMA_Smith_Jane_12.17.1950_OMWb.pdf"
 
     # with open(formUrl,'rb') as pdf_file:
     #     bytes_data = pdf_file.read()
-    bytes_data = download_blob_to_file(blob_service_client,container_name)
 
-    ocr_utils.get_ocr_output(ocr_mode="cloud",document=bytes_data)
+    if context["params"]:
+        mode = context["params"]["ocr_mode"]
+        container_name = context["params"]["container_name"]
+        folder_path = context["params"]["folder_path"]
+        blob_name = context["params"]["file_name"]
+        table_name = context["params"]["ocr_table"]
+
+        file_path = os.path.join(folder_path, blob_name)
+        print("path==================", file_path)
+
+        bytes_data = download_blob_to_file(
+            blob_service_client, container_name, file_path
+        )
+        result = ocr_utils.get_ocr_output(ocr_mode=mode, document=bytes_data)
+        # result["content"] = result["content"].replace("\n","\\n")
+        # result["content"] = json.dumps(result["content"])
+
+        write_output_to_snowflake({"doc_name": blob_name, "extracted_content": result, "table_name":table_name})
+
+
+def write_output_to_snowflake(data):
+
+    print("=========data==========",data)
+    if data is not None:
+        data["extracted_content"]["file_name"] = data["doc_name"]
+        doc_content = data["extracted_content"]
+        del doc_content["content"]
+
+        snf_table = data["table_name"]
+
+        print("================row================", doc_content)
+
+        sql_statement = (
+            rf"insert into {snf_table} (DOC_DETAILS) (select PARSE_JSON('"
+            + json.dumps(doc_content)
+            + "') ) "
+        )
+
+        print("================row================", type(doc_content), sql_statement)
+
+        snf_hook = SnowflakeHook(snowflake_conn_id=SNOWFLAKE_CONN_ID)
+        # res = snf_hook.get_sqlalchemy_engine().execute(statement=sql_statement).fetchone()
+
+        snf_hook.run(sql=sql_statement)
 
 
 def create_connection():
-    adls_details = json.loads(get_kv_secret(KEYVAULT_ADLS_BLOB_SECRET))
     snowflake_details = json.loads(get_kv_secret(KEYVAULT_SNOWFLAKE_SECRET))
 
     snowflake_conn = Connection(
@@ -103,42 +146,19 @@ def create_connection():
         ),
     )
 
-    adls_conn = Connection(
-        conn_id=ADLS_CONN_ID,
-        conn_type="wasb",
-        description="Azure Blob Storage",
-        host=adls_details["host"],
-        extra=json.dumps(
-            {
-                "tenant_id": adls_details["tenant_id"],
-                "connection_string": adls_details["connection_string"],
-            }
-        ),
-    )
-
     snowflake_conn_obj = (
         session.query(Connection)
         .filter(Connection.conn_id == SNOWFLAKE_CONN_ID)
         .first()
     )
-    adls_conn_obj = (
-        session.query(Connection).filter(Connection.conn_id == ADLS_CONN_ID).first()
-    )
 
     print("snowflake - ", type(snowflake_conn_obj))
-    print("adls - ", type(adls_conn_obj))
 
     if snowflake_conn_obj is None:
         print("Creating snowflake connection....")
         session.add(snowflake_conn)
         session.commit()
         print("Snowflake conn established....")
-
-    if adls_conn_obj is None:
-        print("Creating adls connection....")
-        session.add(adls_conn)
-        session.commit()
-        print("ADLS conn established....")
 
 
 def delete_connection():
@@ -147,18 +167,10 @@ def delete_connection():
         .filter(Connection.conn_id == SNOWFLAKE_CONN_ID)
         .first()
     )
-    adls_conn_obj = (
-        session.query(Connection).filter(Connection.conn_id == ADLS_CONN_ID).first()
-    )
-    # sftp_conn_obj = session.query(Connection).filter(Connection.conn_id == SFTP_CONN_ID).first()
 
     if snowflake_conn_obj is not None:
         print("deleting snowflake connection....")
         session.delete(snowflake_conn_obj)
-
-    if adls_conn_obj is not None:
-        print("deleting adls connection....")
-        session.delete(adls_conn_obj)
 
     session.commit()
     session.close()
@@ -172,19 +184,45 @@ with DAG(
         "customer_id": Param(default=120, type=["integer", "string"], min=1, max=255),
         "config_db": Param(default="DEV_OPS_DB", type=["string"], min=1, max=255),
         "config_schema": Param(default="CONFIG", type=["string"], min=1, max=255),
-        "dest_container_name": Param(
+        "container_name": Param(
             default="cont-datalink-dp-shared", type=["string"], min=3, max=255
         ),
-        "source_folder_name": Param(default="OCR_DATA", type=["string"], min=3, max=255),
+        "folder_path": Param(default="OCR_DATA", type=["string"], min=3, max=255),
+        "file_name": Param(
+            default="AetnaMA_Smith_Jane_12.17.1950_OMWb.pdf",
+            type=["string"],
+            min=3,
+            max=255,
+        ),
+        "ocr_table": Param(default="SAMPLE_DB.PUBLIC.OCR_DETAILS", type=["string"], min=1, max=255),
+        "ocr_mode": Param(
+            default="open_source",
+            description="Modes = cloud or open_source",
+            type=["string"],
+            min=3,
+            max=255,
+        )
     },
     # max_active_tasks=2
 ) as dag:
-    
+    create_conn = PythonOperator(
+        task_id="create_connections", python_callable=create_connection
+    )
+
     exec_ocr_process = PythonOperator(
-            task_id="exec_ocr_process",
-            python_callable=get_ocr_details
+        task_id="exec_ocr_process", python_callable=get_ocr_details
+    )
+
+    with TaskGroup(group_id="cleanup") as cleanup:
+        del_conn = PythonOperator(
+            task_id="delete_connections", python_callable=delete_connection
         )
 
-    exec_ocr_process
+        clean_xcom = PythonOperator(
+            task_id="clean_xcom",
+            python_callable=cleanup_xcom,
+            provide_context=True,
+            dag=dag,
+        )
 
-
+    create_conn >> exec_ocr_process >> cleanup
